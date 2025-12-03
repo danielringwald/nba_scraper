19f05d077fe170b7785bcd30f4cc1da056719d0e
Daniel <danielringwald99@gmail.com>
2025-12-03 21:00:15 +0100

    Fix bug in team name information util

--- nba_scraper/team_name_information/team_name_information_util.py
+++ nba_scraper/team_name_information/team_name_information_util.py
@@ -15,16 +15,17 @@ class TeamNameInformationUtil:
 
         team_ids: list[str] = Utils.to_list(team_id)
 
-        all_teams = self.team_name_repository.get_all_teams_information()
+        all_teams_information = self.team_name_repository.get_all_teams_information()
 
-        # All teams have unqieu last 2 digits, i.e. to get the last two we can use modulo 100
+        # All teams have unique last 2 digits, i.e. to get the last two we can use modulo 100
         team_number_representation = [
-            int(team_id[tic.TEAM_ID]) % 100 for team_id in all_teams]
+            int(team_id[tic.TEAM_ID]) % 100 for team_id in all_teams_information]
 
         min_team_number = min(team_number_representation)
+
         result = []
-        for team_id, team_number in zip(all_teams, team_number_representation):
-            if team_id in team_ids:
+        for team_information, team_number in zip(all_teams_information, team_number_representation):
+            if team_information[tic.TEAM_ID] in team_ids:
                 result.append(team_number - min_team_number)
 
         return result
