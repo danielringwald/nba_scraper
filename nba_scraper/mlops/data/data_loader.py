89a5aaf97353244bb355096e5e8e65ea2691309b
Daniel <danielringwald99@gmail.com>
2025-12-13 19:25:08 +0100

    Format

--- nba_scraper/mlops/data/data_loader.py
+++ nba_scraper/mlops/data/data_loader.py
@@ -12,7 +12,6 @@ from nba_scraper.mlops.pipeline.config import LastNGamesFeatures as lngf
 
 HOME_WINNER_COLUMN = "home_team_wins"
 HOME_AWAY_PREFIXES = ['HOME', 'AWAY']
-        
 
 
 class DataLoader:
@@ -29,7 +28,7 @@ class DataLoader:
     def feature_engineer(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.copy()
 
-        # 1. Append winner column  
+        # 1. Append winner column
         df = self._append_season_game_home_winner_df(df)
 
         # 2. Clean numeric columns
@@ -47,50 +46,56 @@ class DataLoader:
                             prefix=HOME_AWAY_PREFIXES, prefix_sep='_')
 
         return df
-    
+
     def feature_engineer_last_n_winner(self, df: pd.DataFrame, n_games: int = 5) -> pd.DataFrame:
         df = df.copy()
 
         # 1. Append winner column
         df = self._append_season_game_home_winner_df(df)
-        
+
         # 2. For all teams, for each last n_games games, check the outcome of the next
         team_abbreviation = "CLE"
-        
+
         sub_df = df.loc[
-            (df[sgc.HOME_TEAM_ABBREVIATION] == team_abbreviation) | (df[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation)
-            ].sort_values(sgc.DATE, ascending=True)
+            (df[sgc.HOME_TEAM_ABBREVIATION] == team_abbreviation) | (
+                df[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation)
+        ].sort_values(sgc.DATE, ascending=True)
 
         for i in range(n_games, len(sub_df)):
             current_game = sub_df.iloc[n_games]
-            last_n_games = sub_df.iloc[i - n_games : n_games]
-            
+            last_n_games = sub_df.iloc[i - n_games: n_games]
+
             print(current_game)
-            print(last_n_games[[sgc.DATE, sgc.HOME_TEAM_ABBREVIATION, sgc.HOME_TEAM_SCORE, sgc.AWAY_TEAM_ABBREVIATION, sgc.AWAY_TEAM_SCORE]])
-            
+            print(last_n_games[[sgc.DATE, sgc.HOME_TEAM_ABBREVIATION,
+                  sgc.HOME_TEAM_SCORE, sgc.AWAY_TEAM_ABBREVIATION, sgc.AWAY_TEAM_SCORE]])
+
             row = {}
             row[lngf.WINNER] = int((current_game[HOME_WINNER_COLUMN] == 1 and current_game[sgc.HOME_TEAM_ABBREVIATION] == team_abbreviation) or
-            (current_game[HOME_WINNER_COLUMN] == 0 and current_game[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation))
-            row[lngf.DATE_DAY] = datetime.date.fromisoformat(str(current_game[sgc.DATE])).day
-            row[lngf.DATE_MONTH] = datetime.date.fromisoformat(str(current_game[sgc.DATE])).month
-            row[lngf.DATE_YEAR] = datetime.date.fromisoformat(str(current_game[sgc.DATE])).year
-            
+                                   (current_game[HOME_WINNER_COLUMN] == 0 and current_game[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation))
+            row[lngf.DATE_DAY] = datetime.date.fromisoformat(
+                str(current_game[sgc.DATE])).day
+            row[lngf.DATE_MONTH] = datetime.date.fromisoformat(
+                str(current_game[sgc.DATE])).month
+            row[lngf.DATE_YEAR] = datetime.date.fromisoformat(
+                str(current_game[sgc.DATE])).year
+
             for i in range(n_games):
                 n_game = last_n_games.iloc[n_games - i - 1]
                 row[lngf.NTH_GAME_PREFIX + str(i + 1)] = int((n_game[HOME_WINNER_COLUMN] == 1 and n_game[sgc.HOME_TEAM_ABBREVIATION] == team_abbreviation) or
-                    (n_game[HOME_WINNER_COLUMN] == 0 and n_game[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation))
-            
-            print(pd.DataFrame(row, index = [team_abbreviation + str(current_game[sgc.DATE])]))
-            return pd.DataFrame()
-            
-        return df
+                                                             (n_game[HOME_WINNER_COLUMN] == 0 and n_game[sgc.AWAY_TEAM_ABBREVIATION] == team_abbreviation))
 
+            print(pd.DataFrame(row, index=[
+                  team_abbreviation + str(current_game[sgc.DATE])]))
+            return pd.DataFrame()
+
+        return df
 
     def _append_season_game_home_winner_df(self, df: pd.DataFrame) -> pd.DataFrame:
         df[HOME_WINNER_COLUMN] = (df[sgc.HOME_TEAM_SCORE]
                                   > df[sgc.AWAY_TEAM_SCORE]).astype(int)
 
         return df
+
 
 if __name__ == "__main__":
     loader = DataLoader()
